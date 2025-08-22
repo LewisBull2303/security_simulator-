36a62ff7814f072dca987d09743f87cf3b239a3d
Lewis Bull <lewisb@aldingbourne.org>
2025-08-22 15:22:52 +0100

    Finalised the project for deployment

--- security_sim/settings.py
+++ security_sim/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'outlookreal-3a6998ac5eab.herokuapp.com']
 
