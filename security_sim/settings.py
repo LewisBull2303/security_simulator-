c03bbca4b3236f759af4c651b4442a729ba16aed
Lewis Bull <lewisb@aldingbourne.org>
2025-08-22 12:46:11 +0100

    Added the postgresql database

--- security_sim/settings.py
+++ security_sim/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'security_sim.wsgi.app'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_name',                      
-        'USER': 'db_user',
-        'PASSWORD': 'db_user_password',
-        'HOST': '',
-        'PORT': 'db_port_number',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
